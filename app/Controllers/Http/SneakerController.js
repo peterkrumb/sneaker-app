3a2e0f7d6981a2bb043e9f8cec71f4ad19de17f6
peterkrumb <petekrumb@gmail.com>
2020-03-30 21:35:49 -0500

    initial commit

--- app/Controllers/Http/SneakerController.js
+++ app/Controllers/Http/SneakerController.js
@@ -1,49 +1,100 @@
-'use strict'
-const Sneaker = use('App/Models/Sneaker')
+"use strict";
+const Sneaker = use("App/Models/Sneaker");
 
 class SneakerController {
-    async index({view}){
-        let allSneakers = await Sneaker.all()
-        allSneakers = allSneakers.toJSON()
-        let viewData = {
-            allSneakers
-        }
-        console.log(viewData);
-        
-        return view.render('sneakers/index', viewData)
-    }
-    async create({view}){
-        return view.render('sneakers/create')
-    }
-    async store({request, response}){
-        try{
+  async index({ view }) {
+    try {
+      let allSneakers = await Sneaker.all();
+      allSneakers = allSneakers.toJSON();
+      let viewData = {
+        allSneakers
+      };
+      console.log(viewData);
 
-            const sneaker = new Sneaker()
-            const data = request.post()
-    
-            sneaker.title = data.title
-            sneaker.image = data.image
-            sneaker.description = data.description
-    
-            await sneaker.save()
-            return response.redirect('/')
-        } catch (error) {
-            console.log(error);
-            return 'Sorry there was an error check console'
-        }
+      return view.render("sneakers/index", viewData);
+    } catch (error) {
+      console.log(error);
+      return "Sorry there was an error check console";
     }
-    async show({view}){
-        return view.render('sneakers/show')
+  }
+  async create({ view }) {
+    return view.render("sneakers/create");
+  }
+  async store({ request, response }) {
+    try {
+      const sneaker = new Sneaker();
+      const data = request.post();
+
+      sneaker.title = data.title;
+      sneaker.image = data.image;
+      sneaker.description = data.description;
+
+      await sneaker.save();
+      return response.redirect("/");
+    } catch (error) {
+      console.log(error);
+      return "Sorry there was an error check console";
     }
-    async edit({view}){
-        return view.render('sneakers/edit')
+  }
+  async show({ view, request }) {
+    try {
+      const sneaker = await Sneaker.find(request.params.id);
+      let viewData = {
+        sneaker: sneaker.toJSON()
+      };
+      console.log(viewData);
+      return view.render("sneakers/show", viewData);
+    } catch (error) {
+      console.log(error);
+      return "Sorry there was an error check console";
     }
-    async update(){
-        return "update 1 sneaker in database"
+  }
+
+  async edit({ view, request }) {
+    try {
+      const sneaker = await Sneaker.find(request.params.id);
+      let viewData = {
+        sneaker: sneaker.toJSON()
+      };
+      console.log(viewData);
+
+      return view.render("sneakers/edit", viewData);
+    } catch (error) {
+      console.log(error);
+      return "Sorry there was an error check console";
     }
-    async destroy(){
-        return "delete 1 sneaker in database"
+  }
+  async update({ request, response }) {
+    try {
+      const sneaker = await Sneaker.find(request.params.id);
+      console.log(request.post());
+      const postData = request.post();
+
+      sneaker.title = postData.title;
+      sneaker.image = postData.image;
+      sneaker.description = postData.description;
+      sneaker.save();
+      console.log(sneaker.toJSON());
+      return response.redirect("back");
+    } catch (error) {
+      console.log(error);
+      return "Sorry there was an error check console";
     }
+  }
+  async destroy({ request, response }) {
+    try {
+      const sneaker = await Sneaker.find(request.params.id);
+      console.log(request.post());
+      const postData = request.post();
+
+      sneaker.delete();
+      console.log(sneaker.toJSON());
+      return response.redirect("back");
+    } catch (error) {
+      console.log(error);
+      return "Sorry there was an error check console";
+    }
+  }
 }
 
-module.exports = SneakerController
+module.exports = SneakerController;
